3c708b01af1b5b7abba4c03bcf7a1486b4560898
Mihai Blebea <mihai@getchip.uk>
2020-11-18 23:47:07 +0000

    Adds tasks in config
    Adds tasks

--- config/config.prod.exs
+++ config/config.prod.exs
@@ -1,5 +1,7 @@
 use Mix.Config
 
+alias Codebot.Domain.Worker
+
 config :codebot,
     mysql_user: "root",
     mysql_password: "root",
@@ -7,3 +9,9 @@ config :codebot,
     mysql_host: "srv-captain--platform-db-db",
     mysql_port: 3306,
     mysql_database: "codebot"
+
+config :codebot, Worker,
+    jobs: [
+        {"1,30 8-17 * * *", &Worker.tell_joke/0 },
+        {"0 8-17/1 * * *", &Worker.list_tasks/0 }
+    ]
