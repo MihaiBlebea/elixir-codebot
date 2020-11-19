37c0beade49cd7c5516d2bc8d580f18428a42972
Mihai Blebea <mihai@getchip.uk>
2020-11-19 00:06:45 +0000

    Returns token

--- lib/infrastructure/slack.ex
+++ lib/infrastructure/slack.ex
@@ -75,7 +75,7 @@ defmodule Codebot.Adapter.Slack do
     end
 
     defp get_default_headers() do
-        String.slice(@app_token, 0..-5) |> inspect |> Logger.debug
+        @app_token |> inspect |> Logger.debug
 
         ["Authorization": "Bearer #{ @app_token }", "Content-Type": "application/json"]
     end
